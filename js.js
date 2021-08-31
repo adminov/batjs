ffcb163a9f8ada80d3cfe1e0262ad898a06d43e0
adminov <batishnazik@gmail.com>
2021-08-31 18:51:43 +0600

    'lesson02'

--- js.js
+++ js.js
@@ -1,10 +1,23 @@
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 100000, //Доход за месяц
+    income = 'Freelance', //Доп. доход
+    addExpenses = 'Свет, Газ, Мусор, бензин', //расходы
+    deposit = true,
+    mission = 7000000, //Какую сумму хотите накопить
+    period = 6; // мясяцев
 
-alert('hi');
-console.log('world');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log("Расходы: " + addExpenses.length);
+
+console.log("Период: " + period);
+console.log("цель заработать: " + mission);
+
+console.log(addExpenses.toLowerCase());
+const arrayAddExpenses = addExpenses.split(', ');
+console.log(arrayAddExpenses);
+
+let budgetDay = money / 30;
+console.log(parseInt(budgetDay));
