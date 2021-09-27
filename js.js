8904d70cac30ef6cba6811652faf537cda67befd
adminov <batishnazik@gmail.com>
2021-09-27 21:34:49 +0600

    lesson18Timer

---
+++ js.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    //timer
+    function countTimer(deadLine) {
+        const timerHour = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+            const dateStop = new Date(deadLine).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                second = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            //let day = Math.floor(timeRemaining / 60 / 60 /24);
+            return {timeRemaining, hours, minutes, second};
+        }
+
+        function updateClock() {
+            const timer = getTimeRemaining();
+            timerHour.textContent = ('0' + timer.hours).slice(-2);
+            timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+            timerSeconds.textContent = ('0' + timer.second).slice(-2);
+
+        }
+
+        const timer = getTimeRemaining();
+        if (timer.timeRemaining > 0) {
+            setInterval(updateClock, 1000);
+        } else {
+            timerHour.textContent = '00';
+            timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
+        }
+
+        updateClock();
+
+    }
+
+    countTimer('01 October 2021');
+
+
+
+});
